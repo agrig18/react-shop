c9079157d5c4f2347e34b48ef01fa5b487837477
agrig18 <agrig18@freeuni.edu.ge>
2022-10-14 18:23:15 +0400

    uppercase cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -13,7 +13,7 @@ class CartPage extends Component {
     const { state, checkout } = this.context;
     return state.cartItems.length ? (
       <S.SCartPageWrapper>
-        <S.SCartTitle>{"Cart"}</S.SCartTitle>
+        <S.SCartTitle>CART</S.SCartTitle>
         <S.SBorder />
         {state.cartItems.map((item) => (
           <div>
